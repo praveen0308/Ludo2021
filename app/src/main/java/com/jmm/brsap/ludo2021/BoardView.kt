6633994e040007e973f935fc01074b23b3588164
ADMIN <praveen0308>
2021-07-31 12:04:15 +0530

    Initial commit

---
+++ app/src/main/java/com/jmm/brsap/ludo2021/BoardView.kt
@@ -0,0 +1,14 @@
+package com.jmm.brsap.ludo2021
+
+import android.content.Context
+import android.graphics.Canvas
+import android.util.AttributeSet
+import android.view.View
+
+class BoardView(context: Context?, attrs: AttributeSet?) : View(context, attrs) {
+
+    override fun onDraw(canvas: Canvas?) {
+        super.onDraw(canvas)
+
+    }
+}
